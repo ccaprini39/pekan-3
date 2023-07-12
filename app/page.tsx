391e022cf9aa8a15277f3c6a152fec24a1831682
Collin Caprini <collin.caprini@agile5technologies.com>
2023-07-12 10:09:35 -0400

    gross import in useEffect

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
 'use client'
-import { Excalidraw } from "@excalidraw/excalidraw";
+// import { Excalidraw } from "@excalidraw/excalidraw";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  // const [Excalidraw, setExcalidraw] = useState(null);
-  // useEffect(() => {
-  //   import("@excalidraw/excalidraw").then((comp) => setExcalidraw(comp.Excalidraw));
-  // }, []);
+  const [Excalidraw, setExcalidraw] = useState(null);
+  useEffect(() => {
+    import("@excalidraw/excalidraw").then((comp) => setExcalidraw(comp.Excalidraw));
+  }, []);
 
   return (
     <div className="h-screen">
       <h1
         className="text-center text-4xl font-bold text-gray-800"
       >
+        Excalidraw
       </h1>
-      <Excalidraw theme="dark" />
-
+      <>{Excalidraw && <Excalidraw theme="dark" />}</>
     </div>
   )
 }
