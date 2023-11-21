7f5c6189481a6ba12e8fb28b96ea83a0ce204792
Collin Caprini <collin.caprini@agile5technologies.com>
2023-11-21 09:57:13 -0500

    hopefully fixing tmp

--- app/speech/server-components.ts
+++ app/speech/server-components.ts
@@ -1,27 +1,27 @@
 'use server'
 
 import fs from "fs";
-import { pipeline } from "stream";
-import { promisify } from "util";
+
+const os = require('os');
 import path from "path";
 import OpenAI from "openai";
 
 const openai = new OpenAI();
 
 
-const directory = path.resolve("/");
-console.log('directory', directory);
-const speechFile = path.resolve("/new-speech-2.mp3");
 
-export async function getSpeech() {
-  const mp3 = await openai.audio.speech.create({
-    model: "tts-1",
-    voice: "alloy",
-    input: "Today is a wonderful day to build something people love!",
-  });
-  console.log(speechFile);
-  const buffer = Buffer.from(await mp3.arrayBuffer());
-  await fs.promises.writeFile(speechFile, buffer);
+function getTempDirectory() {
+  if (process.env.VERCEL || process.env.NOW_REGION) {
+    return '/tmp';
+  }
+  // For local development, check the OS
+  if (os.platform() === 'win32') {
+    // Windows path (adjust as needed)
+    return '';
+  } else {
+    // Unix-like systems (Linux, macOS)
+    return '/tmp';
+  }
 }
 
 export async function createSpeech(text: string) {
@@ -33,7 +33,7 @@ export async function createSpeech(text: string) {
     'nova', //computer sounding worman
     'shimmer', //strong sounding female, slightly robotic
   ]
-  const speechFile = path.resolve(process.cwd(), 'new-speech-2.mp3')
+  const speechFile = path.resolve(process.cwd(), getTempDirectory(), 'new-speech-2.mp3')
   const mp3 = await openai.audio.speech.create({
     model: "tts-1",
     voice: "alloy",
@@ -48,7 +48,7 @@ export async function createSpeech(text: string) {
 //the file will have been created with the createSpeech function
 export async function serveSpeech(filename: string) {
   try {
-    const speechFile = path.resolve(process.cwd() + "/" + filename);
+    const speechFile = path.resolve(process.cwd() + getTempDirectory() + "/" + filename);
     const stream = fs.createReadStream(speechFile);
     const chunks: any[] = [];
     for await (const chunk of stream) {
@@ -59,7 +59,7 @@ export async function serveSpeech(filename: string) {
     return `data:audio/mpeg;base64,${base64}`;
   } catch (error) {
     console.log(error);
-    const speechFile = path.resolve(process.cwd() + "/new-speech.mp3");
+    const speechFile = path.resolve(process.cwd() + getTempDirectory() + "/new-speech.mp3");
     const stream = fs.createReadStream(speechFile);
     const chunks: any[] = [];
     for await (const chunk of stream) {
