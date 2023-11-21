8160323bdd97c37f24d24d9f0736eafebda4f67e
Collin Caprini <collin.caprini@agile5technologies.com>
2023-11-21 18:39:48 -0500

    ???

--- app/speech/server-components.ts
+++ app/speech/server-components.ts
@@ -34,6 +34,7 @@ export async function createSpeech(text: string) {
     'shimmer', //strong sounding female, slightly robotic
   ]
   const speechFile = path.resolve(getTempDirectory(), 'new-speech-2.mp3')
+  console.log('here in the create speech function')
   const mp3 = await openai.audio.speech.create({
     model: "tts-1",
     voice: "alloy",
@@ -48,7 +49,8 @@ export async function createSpeech(text: string) {
 //the file will have been created with the createSpeech function
 export async function serveSpeech(filename: string) {
   try {
-    const speechFile = path.resolve(getTempDirectory() + "/" + filename);
+    console.log('here in the serve speech function')
+    const speechFile = path.resolve(getTempDirectory() + filename);
     const stream = fs.createReadStream(speechFile);
     const chunks: any[] = [];
     for await (const chunk of stream) {
@@ -59,7 +61,7 @@ export async function serveSpeech(filename: string) {
     return `data:audio/mpeg;base64,${base64}`;
   } catch (error) {
     console.log(error);
-    const speechFile = path.resolve(getTempDirectory() + "/new-speech.mp3");
+    const speechFile = path.resolve(getTempDirectory() + "new-speech.mp3");
     const stream = fs.createReadStream(speechFile);
     const chunks: any[] = [];
     for await (const chunk of stream) {
